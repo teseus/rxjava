dc94b1e08aeb23315d55679bc158082fe5d2fc3b
hyuksu.pyun <hyuksu.pyun@kurlycorp.com>
2023-04-09 18:44:37 +0900

    ReflectionTest

--- src/main/kotlin/com/test/just/java/finalizeattack/HackedAttack.java
+++ src/main/kotlin/com/test/just/java/finalizeattack/HackedAttack.java
@@ -6,8 +6,8 @@ public class HackedAttack extends Account {
         super(name);
     }
 
-    @Override
-    protected void finalize() throws Throwable {
+//    @Override
+    protected void finalize1() throws Throwable {  //finalize 재정의를 못하게 막아 놓아서 실행되지 않는다.
         this.transfer(1000, "김정은");
         super.finalize();
     }
---
+++ src/main/kotlin/com/test/just/java/reflection/ReflectionTest.java
@@ -0,0 +1,43 @@
+package com.test.just.java.reflection;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class ReflectionTest {
+    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
+        // 클래스 이름으로부터 클래스 객체를 가져옵니다.
+        Class<?> classObj = MyClass.class;
+
+        // 클래스의 모든 필드를 가져옵니다.
+        Field[] fields = classObj.getDeclaredFields();
+        System.out.println("Fields of MyClass:");
+        for (Field field : fields) {
+            System.out.println("- " + field.getName());
+        }
+
+        MyClass myClass = new MyClass();
+        // 클래스의 모든 메소드를 가져옵니다.
+        Method[] methods = classObj.getDeclaredMethods();
+        System.out.println("\nMethods of MyClass:");
+        for (Method method : methods) {
+            System.out.println("- " + method.getName());
+            method.setAccessible(true);
+            method.invoke(myClass);
+        }
+
+
+    }
+}
+
+class MyClass {
+    private int field1;
+    public String field2;
+
+    public void method1() {
+        System.out.println("method1");
+    }
+    private void method2() {
+        System.out.println("method2");
+    }
+}
